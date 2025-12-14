87d693b632cc802c656bd10a56820d8930d131bb
Ziegiel <DziegielDK@gmail.com>
2025-12-14 23:34:45 +0100

    refactored print function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, refactor_count_characters
 
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
@@ -10,7 +10,11 @@ def main():
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
     char_count = count_characters(book_text)
-    print(char_count)
+    refactored_char_count = refactor_count_characters(char_count)
+    
+    for item in refactored_char_count:
+        if item["char"].isalpha():
+            print(f"'{item['char']}': {item['num']}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -10,5 +10,21 @@ def count_characters(text):
             charecter_count[char] += 1
         else:
             charecter_count[char] = 1
-    
+
     return charecter_count
+def sort_on(item):
+    return item["num"]
+
+def refactor_count_characters(dict_of_characters):
+    refactored_list = []
+
+    for key, value in dict_of_characters.items():
+        refactored_list.append({
+            "char": key, 
+            "num": value,
+            })
+
+    refactored_list.sort(key=sort_on, reverse=True)
+
+    return refactored_list
+
